cd03cc88596c1df9af3bcb896d2b7e0c997dfd1a
delgus <alexdolgovak47@mail.ru>
2020-03-01 17:45:57 +0300

    test with postgresql

--- calc_test.go
+++ calc_test.go
@@ -1,64 +1,113 @@
-package calc
+package main
 
 import (
 	"testing"
+
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+	"github.com/shopspring/decimal"
 )
 
-func TestSumPrice(t *testing.T) {
-	type args struct {
-		x float64
-		y float64
-	}
-	tests := []struct {
-		name string
-		args args
-		want float64
-	}{
-		{name: "case1", args: args{x: 0.001, y: 0.003}, want: 0.00},
-		{name: "case2", args: args{x: 0.002, y: 0.003}, want: 0.01},
-		{name: "case3", args: args{x: 0.00099, y: 0.00401}, want: 0.01},
-		{name: "case4", args: args{x: 0.00099, y: 0.00400}, want: 0.00},
-		{name: "case5", args: args{x: 0.99, y: 0.01}, want: 1.00},
-		{name: "case6", args: args{x: 0.99, y: 0.001}, want: 0.99},
-		{name: "case7", args: args{x: 0.004, y: 0.99}, want: 0.99},
-		{name: "case8", args: args{x: 0.005, y: 0.99}, want: 1.00},
-		{name: "case9", args: args{x: -0.001, y: -0.003}, want: 0.00},
-		{name: "case10", args: args{x: -0.002, y: -0.003}, want: -0.01},
-		{name: "case11", args: args{x: -0.00099, y: -0.00401}, want: -0.01},
-		{name: "case12", args: args{x: -0.00099, y: -0.00400}, want: 0.00},
-		{name: "case13", args: args{x: -0.99, y: -0.01}, want: -1.00},
-		{name: "case14", args: args{x: -0.99, y: -0.001}, want: -0.99},
-		{name: "case15", args: args{x: -0.004, y: -0.99}, want: -0.99},
-		{name: "case16", args: args{x: -0.005, y: -0.99}, want: -1.00},
-		{name: "case17", args: args{x: 9999.9999, y: 9999.9999}, want: 20000.00},
-		{name: "case18", args: args{x: -9999.9999, y: -9999.9999}, want: -20000.00},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := SumPrice(tt.args.x, tt.args.y); got != tt.want {
-				t.Errorf("unexpected result %v, want %v", got, tt.want)
-			}
-		})
+type testRow struct {
+	NumericParam decimal.Decimal `db:"numeric_param"`
+	RealParam    decimal.Decimal `db:"real_param"`
+}
+
+type testCase struct {
+	x, y, wantFloat                   float64
+	name, wantString, wantStringFixed string
+}
+
+func newTestCase(name string, x, y, wantFloat float64, wantString, wantStringFixed string) *testCase {
+	return &testCase{
+		name:            name,
+		x:               x,
+		y:               y,
+		wantString:      wantString,
+		wantStringFixed: wantStringFixed,
+		wantFloat:       wantFloat,
 	}
 }
 
-func TestSumQuantity(t *testing.T) {
-	type args struct {
-		x float64
-		y float64
+func (tc *testCase) save(db *sqlx.DB) error {
+	query := "insert into records (name, numeric_param, real_param) values ($1, $2, $3),($1, $4, $5)"
+	_, err := db.Exec(query, tc.name, tc.x, tc.x, tc.y, tc.y)
+	return err
+}
+
+func (tc *testCase) check(db *sqlx.DB, t *testing.T) {
+	var rows []testRow
+	query := "select numeric_param, real_param from records where name = $1"
+	if err := db.Select(&rows, query, tc.name); err != nil {
+		t.Error(err)
 	}
-	tests := []struct {
-		name string
-		args args
-		want float64
-	}{
-		// TODO: Add test cases.
+
+	var numeric, real decimal.Decimal
+	for i := range rows {
+		numeric = numeric.Add(rows[i].NumericParam)
+		real = real.Add(rows[i].RealParam)
 	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := SumQuantity(tt.args.x, tt.args.y); got != tt.want {
-				t.Errorf("SumQuantity() = %v, want %v", got, tt.want)
-			}
-		})
+
+	if f, _ := numeric.Round(2).Float64(); f != tc.wantFloat {
+		t.Errorf(`%s: unexpected float64 from numeric - %v, expected %v`, tc.name, f, tc.wantFloat)
+	}
+	if s := numeric.Round(2).String(); s != tc.wantString {
+		t.Errorf(`%s: unexpected string from numeric - %v, expected %v`, tc.name, s, tc.wantString)
+	}
+	if sf := numeric.Round(2).StringFixed(2); sf != tc.wantStringFixed {
+		t.Errorf(`%s: unexpected string fixed from numeric - %v, expected %v`, tc.name, sf, tc.wantStringFixed)
+	}
+
+	if f, _ := real.Round(2).Float64(); f != tc.wantFloat {
+		t.Errorf(`%s: unexpected float64 from real - %v, expected %v`, tc.name, f, tc.wantFloat)
+	}
+	if s := real.Round(2).String(); s != tc.wantString {
+		t.Errorf(`%s: unexpected string from real - %v, expected %v`, tc.name, s, tc.wantString)
+	}
+	if sf := real.Round(2).StringFixed(2); sf != tc.wantStringFixed {
+		t.Errorf(`%s: unexpected string fixed from real - %v, expected %v`, tc.name, sf, tc.wantStringFixed)
+	}
+}
+
+func TestAll(t *testing.T) {
+	// open connection with postgres
+	db, err := sqlx.Open("postgres", "user=postgres password=123456 dbname=postgres host=localhost port=5000 sslmode=disable")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	defer db.Close()
+	defer db.Exec("delete from records")
+
+	testCases := []*testCase{
+		newTestCase("c1", 0.001, 0.003, 0.00, "0", "0.00"),
+		newTestCase("c2", 0.002, 0.003, 0.01, "0.01", "0.01"),
+		newTestCase("c3", 0.00099, 0.00401, 0.01, "0.01", "0.01"),
+		newTestCase("c4", 0.00099, 0.00400, 0.00, "0", "0.00"),
+		newTestCase("c5", 0.99, 0.01, 1.00, "1", "1.00"),
+		newTestCase("c6", 0.99, 0.001, 0.99, "0.99", "0.99"),
+		newTestCase("c7", 0.004, 0.99, 0.99, "0.99", "0.99"),
+		newTestCase("c8", 0.005, 0.99, 1.00, "1", "1.00"),
+		newTestCase("c9", -0.001, -0.003, 0.00, "0", "0.00"),
+		newTestCase("c10", -0.002, -0.003, -0.01, "-0.01", "-0.01"),
+		newTestCase("c11", -0.00099, -0.00401, -0.01, "-0.01", "-0.01"),
+		newTestCase("c12", -0.00099, -0.00400, 0.00, "0", "0.00"),
+		newTestCase("c13", -0.99, -0.01, -1.00, "-1", "-1.00"),
+		newTestCase("c14", -0.99, -0.001, -0.99, "-0.99", "-0.99"),
+		newTestCase("c15", -0.004, -0.99, -0.99, "-0.99", "-0.99"),
+		newTestCase("c16", -0.005, -0.99, -1.00, "-1", "-1.00"),
+		newTestCase("c17", 9999.9999, 9999.9999, 20000.00, "20000", "20000.00"),
+		newTestCase("c18", -9999.9999, -9999.9999, -20000.00, "-20000", "-20000.00"),
+	}
+
+	for i := range testCases {
+		if err := testCases[i].save(db); err != nil {
+			t.Error(err)
+			return
+		}
+	}
+
+	for i := range testCases {
+		testCases[i].check(db, t)
 	}
 }
--- init/init.sql
+++ init/init.sql
@@ -1,5 +1,6 @@
 CREATE TABLE records (
     id serial not null,
+    name varchar(255) not null,
     numeric_param numeric(20,6),
     real_param real
 );
